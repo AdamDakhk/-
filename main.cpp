507f753ff3d5dc406daf55271cbd988234854f7f
AdamDakhk <adamka62ui1234@gmail.com>
2020-09-14 08:43:02 +0000

    задача 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+int a;
+int b;
+cout <<"Пожалуйста введите 2 числа"<<endl;
+cin >>a;
+cin>>b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout <<"Числа махнулись местами:"<<endl <<b <<endl <<a;
+return 0;
+
 }
